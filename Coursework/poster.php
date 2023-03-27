9f528d0f56b4344ea438dc6c7541afda1d9fc4dc
waxxy2022 <123545510+waxxy2022@users.noreply.github.com>
2023-03-27 07:58:21 +0100

    Add files via upload

---
+++ Coursework/poster.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Insert Page page</title>
+</head>
+
+<body>
+	<center>
+		<?php
+
+		// servername => localhost
+		// username => root
+		// password => empty
+		// database name => staff
+		$conn = mysqli_connect("localhost", "root", "", "usernew");
+
+		
+		// Check connection
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		};
+		
+		// Taking all 3 values from the form data(input)
+		$Postme = $_POST['Postme'];
+        $Texter = $_POST['Texter'];
+		// $fileToUpload = $_POST['fileToUpload'];
+        
+		
+		 
+
+		
+		// Performing insert query execution
+		// here our table name is college
+		$sql = "INSERT INTO colle VALUES ('$Postme', '$Texter')";
+		
+		if(mysqli_query($conn, $sql)){
+			echo "<h3>Your post will be live once our admin approve it</h3>";
+
+			
+		} else{
+			echo "ERROR: Hush! Sorry $sql. "
+				. mysqli_error($conn);
+		}
+		
+		// Close connection
+		mysqli_close($conn);
+		$target_dir = "uploads/";
+		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		$uploadOk = 1;
+		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+		// Check if image file is a actual image or fake image
+		if(isset($_POST["submit"])) {
+		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		if($check !== false) {
+			echo "File is an image - " . $check["mime"] . ".";
+			$uploadOk = 1;
+		} else {
+			echo "File is not an image.";
+			$uploadOk = 0;
+		}
+		}
+
+		?>
+	</body>
+
+	</html>
---
+++ Coursework/user.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Insert Page page</title>
+</head>
+
+<body>
+	<center>
+		<?php
+
+		// servername => localhost
+		// username => root
+		// password => empty
+		// database name => staff
+		$conn = mysqli_connect("localhost", "root", "", "user");
+		
+		// Check connection
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		};
+		
+		// Taking all 3 values from the form data(input)
+		$Username = $_POST['Username'];
+		$Email = $_POST['Email'];
+		$Password = $_POST['Password'];
+		
+		
+		// Performing insert query execution
+		// here our table name is college
+		$sql = "INSERT INTO colle VALUES ('$Username',
+			'$Email','$Password')";
+		
+		if(mysqli_query($conn, $sql)){
+			echo "<h3>data stored in a database successfully."
+				. " Please browse your localhost php my admin"
+				. " to view the updated data</h3>";
+
+			// echo nl2br("\n$first_name\n $last_name\n "
+			// 	. "$gender\n $address\n $email");
+		} else{
+			echo "ERROR: Hush! Sorry $sql. "
+				. mysqli_error($conn);
+		}
+		
+		// Close connection
+		mysqli_close($conn);
+		?>
+	</center>
+</body>
+
+</html>
